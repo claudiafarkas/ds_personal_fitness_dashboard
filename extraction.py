5964189047a53ab58d25d939a37f838068ea8503
Claudia Farkas <claudia.farkas@outlook.com>
2023-11-07 21:16:03 -0500

    finished first version of extractions extractions.p

--- extraction.py
+++ extraction.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # Load and parse the XML file
 tree = ET.parse('/Users/Claudia/Library/CloudStorage/OneDrive-Personal/personal_fitness_dashboard/apple_health_export/export.xml')
-root = tree.getroot()
+root = tree.getroot() 
 
 # Define the workout types you are interested in
 # Add to this list any other types that represent workout data
